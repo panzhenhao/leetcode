c9aa58cc26a228bd8ad4750591f93959e9e2df60
pzh <pzh@DESKTOP-1LPLP0E>
2017-03-06 13:44:00 +0800

    412. Fizz Buzz

---
+++ src/id412_FizzBuzz/Solution.java
@@ -0,0 +1,28 @@
+package id412_FizzBuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	public List<String> fizzBuzz(int n) {
+		List<String> res = new ArrayList<String>();
+		for(int i=1; i<=n ; i++){
+			if(i%15==0){
+				res.add("FizzBuzz");
+			}else if(i%3==0){
+				res.add("Fizz");
+			}else if(i%5==0){
+				res.add("Buzz");
+			}else{
+				res.add(String.valueOf(i));
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		List<String> re = solution.fizzBuzz(15);
+		System.out.println(re);
+	}
+}
