5c382e5951742708277ff0f356bb588840aa3a0c
pzh <pzh@DESKTOP-1LPLP0E>
2017-03-26 13:52:40 +0800

    100	Same Tree

---
+++ src/id100_SameTree/Solution.java
@@ -0,0 +1,17 @@
+package id100_SameTree;
+import util.TreeNode;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
