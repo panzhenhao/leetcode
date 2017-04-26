9d2a3359398bab33a44c0f9c99c6180aae703048
pzh <pzh@DESKTOP-1LPLP0E>
2017-04-26 14:57:13 +0800

    35. Search Insert Position

---
+++ src/id35_SearchInsertPosition/Solution.java
@@ -0,0 +1,30 @@
+package id35_SearchInsertPosition;
+
+public class Solution {
+	public int searchInsert(int[] nums, int target) {
+		if(nums == null || nums.length == 0)  
+	    {  
+	        return 0;  
+	    }  
+	    int l = 0;  
+	    int r = nums.length-1;  
+	    while(l<=r)  
+	    {  
+	        int mid = (l+r)/2;  
+	        if(nums[mid]==target)  
+	            return mid;  
+	        if(nums[mid]<target)  
+	            l = mid+1;  
+	        else  
+	            r = mid-1;  
+	    }  
+	    return l;  
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[] data = {1,3,5,6};
+		int re = solution.searchInsert(data,4);
+		System.out.println(re);
+	}
+}
